89a6080232ea6f7864b2684fbc63ee85bbe0a569
r3tnuHq3IhgN <nghiephunter.dev.uet@gmail.com>
2022-07-31 22:11:07 +0700

    chapter_12

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,10 @@ class UserMailer < ApplicationMailer
 
     mail to: user.email, subject: t("account_activation")
   end
+
+  def password_reset user
+    @user = user
+
+    mail to: user.email, subject: t("password_reset")
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   USER_ATTRS = %i(name email password password_confirmation).freeze
+  PASSWORD_RESET_ATTRS = %i(password password_confirmation).freeze
   before_save :downcase_email
   before_create :create_activation_digest
 
@@ -52,6 +53,19 @@ class User < ApplicationRecord
 
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < Settings.email_expired.hours.ago
   end
 
   private
